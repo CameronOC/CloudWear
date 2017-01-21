f55528f38980bccdd0572b8a897b92ef5ceaf71a
vijaypmk <vijay.pmk11@gmail.com>
2017-01-21 15:49:39 -0800

    Added thresholding

--- image_anal/rem_bkg.cpp
+++ image_anal/rem_bkg.cpp
@@ -44,7 +44,9 @@ class cloudwear
         roi.height = nc_img1.size().height - (offset_y*2);
         img1 = nc_img1(roi);
         dst.create(img1.size(), img1.type());
-        cvtColor(img1, img1_gray, CV_BGR2GRAY);
+        cvtColor(img1, img1_gray, COLOR_BGR2GRAY);
+        //img1_gray = img1;
+        //img1_gray = Scalar::all(255);
     }
 
     void display_image(Mat i1)
@@ -56,39 +58,70 @@ class cloudwear
 
     void canny_threshold()
     {
-        blur(img1_gray, det_edges, Size(3,3));
+        //blur(img1_gray, det_edges, Size(3,3));
         //Canny(detected_edges, low_thres, low_thres*ratio, kernel_size);
-        Canny(img1_gray, det_edges, low_thres, low_thres*ratio);
-        dst = Scalar::all(0);
+        //Canny(img1_gray, det_edges, low_thres, low_thres*ratio);
+        dst = Scalar::all(255);
+        det_edges = img1_gray;
 
         img1.copyTo(dst, det_edges);
         display_image(dst);
     }
-
-     void gray_thresh()
-     {
+    
+    void gray_thresh()
+    {
+        int a = 0, b = 0; 
+        blur(img1_gray, img1_gray, Size(3,3));
+        //cout << (int)img1_gray.at<uchar>(100, 100)<<endl;
         for(int i=0;i<img1_gray.rows;i++)
         {
             for(int j=0; j<img1_gray.cols;j++)
             {
-                if(img1_gray.at<double>(i,j) >180) 
-                    img1_gray.at<double>(i,j) =255;
+                //if((int)img1_gray.at<uchar>(i,j)>1)
+                //{
+                    a = a + (int)img1_gray.at<uchar>(i,j);
+                    b++;
+                //}
             }
         }
-     }
+        a = a/b;
 
-    //void process_images()
-    //{
+        for(int i=0;i<img1_gray.rows;i++)
+        {
+            for(int j=0; j<img1_gray.cols;j++)
+            {
+                //cout << img1_gray.at<double>(i, j)<<endl;
+                if((int)img1_gray.at<uchar>(i,j) > a - 20) 
+                {
+                    img1_gray.at<uchar>(i,j) = 0;                    
+                }
+            }
+        }
+        cout << a << endl;
+        //blur(img1_gray, img1_gray, Size(3,3));
+        canny_threshold();
+        //display_image(img1_gray);
+    }
+
+    void process_images()
+    {
+        SimpleBlobDetector detector;
+         
+        // Detect blobs.
+        vector<KeyPoint> keypoints;
+        detector.detect(img1_gray, keypoints);
+    }
 
 }ob;
 
 int main(int argc, char** argv)
 {
     string a;
-    a = "blue_shirt.JPG";
+    a = "track_pants.JPG";
     ob.read_image(a);
     ob.gray_thresh();
-    ob.display_image(ob.img1_gray);
+    //ob.process_images();
+    //ob.display_image(ob.img1_gray);
     //ob.canny_threshold();
 }
 
