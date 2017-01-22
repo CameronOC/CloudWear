0efd1072f2fcc0319638da3d6155568934f724c6
Cameron Johnson <cojohnso@ucsc.edu>
2017-01-22 01:41:30 -0800

    video

--- source/static/main.js
+++ source/static/main.js
@@ -110,7 +110,41 @@ function BottombackUrl() {
    $("#bottom").attr('src',BottomnextUrl());
  });
 });
+<video autoplay></video>
+<img src="">
+<canvas style="display:none;"></canvas>
 
+<script>
+ var video = document.querySelector('video');
+ var canvas = document.querySelector('canvas');
+ var ctx = canvas.getContext('2d');
+ var localMediaStream = null;
+
+ function snapshot() {
+   if (localMediaStream) {
+     ctx.drawImage(video, 0, 0);
+     // "image/webp" works in Chrome.
+     // Other browsers will fall back to image/png.
+     document.querySelector('img').src = canvas.toDataURL('image/webp');
+   }
+ }
+
+ video.addEventListener('click', snapshot, false);
+
+ // Not showing vendor prefixes or code that works cross-browser.
+ navigator.getUserMedia({video: true}, function(stream) {
+   video.src = window.URL.createObjectURL(stream);
+   localMediaStream = stream;
+ }, errorCallback);
+</script>
+
+
+<div style="text-align:center;">
+ <video id="screenshot-stream" class="videostream" autoplay></video>
+ <img id="screenshot" src="">
+ <canvas id="screenshot-canvas" style="display:none;"></canvas>
+ <p><button id="screenshot-button">Capture</button> <button id="screenshot-stop-button">Stop</button></p>
+</div>
 	$(function() {
 
 		var	$window = $(window),
